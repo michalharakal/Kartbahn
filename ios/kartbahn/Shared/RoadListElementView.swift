2b806def734daa3f963466aba9b5c6d90842ff7f
Alexander von Below <Alex@vonBelow.Com>
2021-08-26 11:01:31 +0200

    Attempt to get the count update

--- ios/kartbahn/Shared/RoadListElementView.swift
+++ ios/kartbahn/Shared/RoadListElementView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 import shared
 
 struct RoadListElementDetail: View {
-    @State var road: RoadViewModelData
+    @State var name: String
+    @State var warningsCount: Int
     var body: some View {
         HStack (alignment: .center) {
-            AutobahnNumberView(number: road.name)
+            AutobahnNumberView(number: name)
             Spacer()
 
             Text("Verkehrsmeldungen:")
-            if road.warningsCount == 0 {
+            if warningsCount == 0 {
                 Text("Keine")
             } else {
-                Text("\(road.warningsCount)")
+                Text("\(warningsCount)")
             }
         }
     }
@@ -31,7 +32,7 @@ struct RoadListElementView: View {
         let warnings = WarningsLivePublisher(name: road.name)
 
         NavigationLink(destination: WarningsListView(warningsPublisher: warnings)) {
-            RoadListElementDetail(road: road)
+            RoadListElementDetail(name: road.name, warningsCount: warnings.warnings.count)
         }
     }
 }
